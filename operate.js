49b60f554db22c4be5c1b6d9d2b7990ea01bffb9
Mattia <mattia.rizzardi@alice.it>
2018-07-13 16:57:31 +0200

    Function

---
+++ operate.js
@@ -0,0 +1,26 @@
+// Funzione che svlge prende in input gli operandi e svolge l'operazione richiesta:
+
+function operate(numberOne, numberTwo, operation) {
+    
+    if (operation === "+") {
+        return numberOne + numberTwo;
+    }
+    if (operation === "-") {
+        return numberOne - numberTwo;
+    }
+    if (operation === "x") {
+        return numberOne * numberTwo;
+    }
+    if (operation === "÷") {
+        return numberOne / numberTwo;
+    }
+    if (typeof numberOne == String || typeof numberTwo == String) {
+        return null;        
+    }
+    else {
+        return null;
+    }
+    //throw Error(`Unknown operation '${operation}'`);
+}
+
+module.exports = operate;
